eb46924cf6e401d68f0b8b9908d7dc493dbe5b7c
viitorgonzalez <pastor123.vs@gmail.com>
2023-02-13 18:40:36 -0300

    update

--- Cadastro/Pessoa.java
+++ Cadastro/Pessoa.java
@@ -26,11 +26,10 @@ public class Pessoa {
     public int getIdade() {
         return idade;
     }
-    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 
     public void setIdade(int idade) {
         this.idade = idade;
